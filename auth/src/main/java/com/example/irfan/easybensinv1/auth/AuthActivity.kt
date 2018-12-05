01143ea1b2e158698c161956d752774c0fd7e81c
irfan salimi <irfansalimi1@gmail.com>
2018-12-05 11:43:53 +0700

    Initial commit

---
+++ auth/src/main/java/com/example/irfan/easybensinv1/auth/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.example.irfan.easybensinv1.auth
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
